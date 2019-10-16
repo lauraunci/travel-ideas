ea2bdc4e2824cd58b0ff769c68fe3de4d5babd4b
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2019-10-16 14:43:51 -0400

    chance routs for redirect

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
  router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/trips',
+    successRedirect : '/',
     failureRedirect : '/'
   }
 ));
